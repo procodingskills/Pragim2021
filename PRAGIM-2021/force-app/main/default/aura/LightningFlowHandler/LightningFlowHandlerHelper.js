03cc6198f2d6860a3cb54173789786c6690d2efb
Lakshmi Deepak Inkurthi <i.lakshmideepak@gmail.com>
2021-03-09 09:11:11 +0530

    Second Update

--- PRAGIM-2021/force-app/main/default/aura/LightningFlowHandler/LightningFlowHandlerHelper.js
+++ PRAGIM-2021/force-app/main/default/aura/LightningFlowHandler/LightningFlowHandlerHelper.js
@@ -1,6 +1,28 @@
 ({
     doInitHandler : function(component, event, helper) {
         const startIndex = component.get("v.startIndex");
-        const componentName = component.get("v.componentList")[startIndex];
+        const componentName = "c:"+component.get("v.componentList")[startIndex];
+        alert("componentName : "+componentName);
+        let params = this.setParams(component, event, helper);
+        $A.createComponent(componentName ,params , function(newComponent , status , errorMessage){
+            if(status === "SUCCESS"){
+                let body = component.get("v.body");
+                body.push(newComponent);
+                component.set("v.body",body);
+            }
+            else {
+                alert ("Craete CMP ERROR : "+JSON.stringify(errorMessage));
+            }
+        });
+    },
+    setParams : function(component, event, helper){
+        return {
+            "sObjectName" : component.getReference("v.sObjectName"),
+            "sObjectId" : component.getReference("v.sObjectId"),
+            "componentList" : component.getReference("v.componentList"),
+            "currentPageIndex" : component.getReference("v.currentPageIndex"),
+            "startIndex" : component.getReference("v.startIndex"),
+            "actionName" : component.getReference("v.actionName"),
+        };
     }
 })
