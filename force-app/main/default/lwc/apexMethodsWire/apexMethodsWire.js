da604cbd40e851262586133d3e8677e1e291ae2e
Lakshmi Deepak Inkurthi <i.lakshmideepak@gmail.com>
2021-03-25 08:45:48 +0530

    Updated

--- force-app/main/default/lwc/apexMethodsWire/apexMethodsWire.js
+++
@@ -1,6 +0,0 @@
-import { LightningElement,wire } from 'lwc';
-import getAccounts from '@salesforce/apex/ProcodingskillsCtr.getAccounts';
-
-export default class ApexMethodsWire extends LightningElement {
-    @wire(getAccounts) accounts;
-}
